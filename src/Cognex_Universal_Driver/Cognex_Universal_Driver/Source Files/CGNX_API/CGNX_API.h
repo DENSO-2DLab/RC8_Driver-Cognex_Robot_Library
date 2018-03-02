f7fe82bad756af5f565d36e0c44f598c074b8567
Carlos A. Lopez <carlos_lopez@densodrive.com>
2018-03-02 15:16:33 -0800

    Update on API Comm settings (Moving it to header file  and adding the spec for vision system)

--- src/Cognex_Universal_Driver/Cognex_Universal_Driver/Source Files/CGNX_API/CGNX_API.h
+++ src/Cognex_Universal_Driver/Cognex_Universal_Driver/Source Files/CGNX_API/CGNX_API.h
@@ -1,5 +1,8 @@
 '!TITLE "Cognex API Setting"
 
-#Define CGNX_API_VER	"1.3.1"
+#Define _VIS_HMI_COM_ 8
+#Define _VIS_DEV_COM_ 9
+
+#Define CGNX_API_VER	"1.3.2"
 #Define SUCCEEDED		1
 #Define FAILED			0
