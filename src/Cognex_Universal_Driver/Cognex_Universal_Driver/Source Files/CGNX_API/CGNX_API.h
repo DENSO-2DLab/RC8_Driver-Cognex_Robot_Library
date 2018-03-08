0227a52855c418d829c25ea47130b642c5a3f664
Carlos A. Lopez <carlos_lopez@densodrive.com>
2018-03-07 18:59:29 -0800

    Upated on CGNX_API - Integration of Vision_Lib functions with new DPAM_stdlib function - Minor Bug Fixes

--- src/Cognex_Universal_Driver/Cognex_Universal_Driver/Source Files/CGNX_API/CGNX_API.h
+++ src/Cognex_Universal_Driver/Cognex_Universal_Driver/Source Files/CGNX_API/CGNX_API.h
@@ -1,8 +1,10 @@
 '!TITLE "Cognex API Setting"
 
+#Include <Variant.h>
+
 #Define _VIS_HMI_COM_ 8
 #Define _VIS_DEV_COM_ 9
 
-#Define CGNX_API_VER	"1.6.22"
+#Define CGNX_API_VER	"1.6.23"
 #Define SUCCEEDED		1
 #Define FAILED			0
