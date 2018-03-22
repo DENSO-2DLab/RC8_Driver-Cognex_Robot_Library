0053a29c0eca685330fe2f934ba3cf353b04b951
Carlos A. Lopez <carlos_lopez@densodrive.com>
2018-03-22 15:32:31 -0700

    Issue #5 Update - Added an Offset to Trained Positions.

--- src/Cognex_Universal_Driver/Cognex_Universal_Driver/Source Files/CGNX_API/CGNX_API.h
+++ src/Cognex_Universal_Driver/Cognex_Universal_Driver/Source Files/CGNX_API/CGNX_API.h
@@ -2,9 +2,25 @@
 
 #Include <Variant.h>
 
-#Define _VIS_HMI_COM_ 8
-#Define _VIS_DEV_COM_ 9
+'========================================================================================
+'User Data
+'========================================================================================
 
-#Define CGNX_API_VER	"1.6.25"
+#Define _VIS_HMI_COM_ 	8	'Specify Comm line for Vision HMI
+#Define _VIS_DEV_COM_ 	9	'Specify Comm line for Vision Camera
+
+'Specify Where to start saving Train Positions (default P90)
+#Define _TRAIN_POS_OFFSET_ 	90	
+#Define _TRAIN_POS_0_	(0 + _TRAIN_POS_OFFSET_)
+#Define _TRAIN_POS_1_	(1 + _TRAIN_POS_OFFSET_)
+#Define _TRAIN_POS_2_	(2 + _TRAIN_POS_OFFSET_)
+#Define _TRAIN_POS_3_	(3 + _TRAIN_POS_OFFSET_)
+#Define _TRAIN_POS_4_	(4 + _TRAIN_POS_OFFSET_)
+
+'========================================================================================
+'API Data
+'========================================================================================
+
+#Define CGNX_API_VER	"1.6.26"
 #Define SUCCEEDED		1
 #Define FAILED			0
