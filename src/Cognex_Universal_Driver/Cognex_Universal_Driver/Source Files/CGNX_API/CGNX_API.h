1a6be25bc02cc387ee697078b30d41962e94b718
Carlos A. Lopez <carlos_lopez@densodrive.com>
2018-05-31 11:44:47 -0700

    Issue #7 Implemented (Not Tested)

--- src/Cognex_Universal_Driver/Cognex_Universal_Driver/Source Files/CGNX_API/CGNX_API.h
+++ src/Cognex_Universal_Driver/Cognex_Universal_Driver/Source Files/CGNX_API/CGNX_API.h
@@ -22,6 +22,6 @@
 'API Data
 '========================================================================================
 
-#Define CGNX_API_VER	"1.7.1"
+#Define CGNX_API_VER	"1.7.2"
 #Define SUCCEEDED		1
 #Define FAILED			0
