cbd8b3a89e7ae3c2cb955bf15c2926d01173cde9
Carlos A. Lopez <carlos_lopez@densodrive.com>
2018-05-31 10:33:29 -0700

    Issue #8:  - Using header file to define EasySetup Positions and Tool Offset

--- src/Cognex_Universal_Driver/Cognex_Universal_Driver/Source Files/CGNX_API/CGNX_API.h
+++ src/Cognex_Universal_Driver/Cognex_Universal_Driver/Source Files/CGNX_API/CGNX_API.h
@@ -9,18 +9,19 @@
 #Define _VIS_HMI_COM_ 	8	'Specify Comm line for Vision HMI
 #Define _VIS_DEV_COM_ 	9	'Specify Comm line for Vision Camera
 
-'Specify Where to start saving Train Positions (default P90)
-#Define _TRAIN_POS_OFFSET_ 	90	
-#Define _TRAIN_POS_0_	(0 + _TRAIN_POS_OFFSET_)
-#Define _TRAIN_POS_1_	(1 + _TRAIN_POS_OFFSET_)
-#Define _TRAIN_POS_2_	(2 + _TRAIN_POS_OFFSET_)
-#Define _TRAIN_POS_3_	(3 + _TRAIN_POS_OFFSET_)
-#Define _TRAIN_POS_4_	(4 + _TRAIN_POS_OFFSET_)
+' EasySetup Program Settings
+#Define _WORKING_TOOL_	1							' Tool Offset to be configured 
+#Define _TRAIN_POS_OFFSET_ 	90						' Trained Pos Index Start (default P90)
+#Define _TRAIN_POS_0_	(0 + _TRAIN_POS_OFFSET_)	' Home Pos
+#Define _TRAIN_POS_1_	(1 + _TRAIN_POS_OFFSET_)	' Calibration Start Pos
+#Define _TRAIN_POS_2_	(2 + _TRAIN_POS_OFFSET_)	' Trained Pick Pos (StepID=1)
+#Define _TRAIN_POS_3_	(3 + _TRAIN_POS_OFFSET_)	' Trained VGR(Picked) position (StepID=2)
+#Define _TRAIN_POS_4_	(4 + _TRAIN_POS_OFFSET_)	' Trained Place Pos (StepID=3)
 
 '========================================================================================
 'API Data
 '========================================================================================
 
-#Define CGNX_API_VER	"1.7.0-alpha"
+#Define CGNX_API_VER	"1.7.1"
 #Define SUCCEEDED		1
 #Define FAILED			0
