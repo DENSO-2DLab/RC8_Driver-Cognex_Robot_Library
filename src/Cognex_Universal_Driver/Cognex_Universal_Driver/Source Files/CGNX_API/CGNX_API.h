40129e4016107b56971b224ff6ca2ef6fd9e3b80
Carlos A. Lopez <carlos_lopez@densodrive.com>
2018-06-11 19:19:26 -0700

    Adding Licensing to the Library files

--- src/Cognex_Universal_Driver/Cognex_Universal_Driver/Source Files/CGNX_API/CGNX_API.h
+++ src/Cognex_Universal_Driver/Cognex_Universal_Driver/Source Files/CGNX_API/CGNX_API.h
@@ -1,5 +1,38 @@
 '!TITLE "Cognex API Setting"
 
+' /**
+' * @file    CGNX_API.h
+' * @brief   Cognex API Settings.
+' * @details https://github.com/DENSO-2DLab/RC8_Driver-Cognex_Robot_Library
+' *
+' * @version 2.1.0
+' * @date    2018/06/11
+' * @author  Carlos A. Lopez (DPAM - R&D)
+' *
+' * Software License Agreement (MIT License)
+' *
+' * @copyright Copyright (c) 2018 DENSO 2DLab
+' *
+' * Permission is hereby granted, free of charge, to any person obtaining a copy
+' * of this software and associated documentation files (the "Software"), to deal
+' * in the Software without restriction, including without limitation the rights
+' * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' * copies of the Software, and to permit persons to whom the Software is
+' * furnished to do so, subject to the following conditions:
+' *
+' * The above copyright notice and this permission notice shall be included in
+' * all copies or substantial portions of the Software.
+' *
+' * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+' * THE SOFTWARE.
+' */
+
+
 #Include <Variant.h>
 
 '========================================================================================
@@ -33,7 +66,7 @@
 'API Data
 '========================================================================================
 
-#Define CGNX_API_VER	"2.0.1"
+#Define CGNX_API_VER	"2.1.0"
 #Define SUCCEEDED		1
 #Define FAILED			0
 
